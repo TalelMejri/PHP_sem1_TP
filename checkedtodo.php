22aab6ae8bbb83ed4aedbd340c3938976d03382f
talel <talelmejri8@gmail.com>
2022-10-27 22:59:14 +0100

    check boc

---
+++ checkedtodo.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once "./db_connected/index.php";
+
+$sql=$pdo->prepare("SELECT * from todos WHERE id=?");
+$sql->execute([$_GET['id']]);   
+$todo=$sql->fetch();
+
+$query=$pdo->prepare("UPDATE todos SET complete=:com where id=:idtodo");
+$query->execute(["com" => !$todo['complete'],
+                "idtodo"=>$_GET['id']]);
+header("location:./profiluser") ;
+
+?>
--- profiluser/index.php
+++ profiluser/index.php
@@ -1,10 +1,19 @@
 <?php 
     session_start();
+    require_once "../db_connected/index.php";
     if(!isset($_SESSION['nameuser'])){
         header("location:../login") ;
         exit;
      }
-     require_once "../db_connected/index.php";
+     if(isset($_GET['check'])){
+        extract($_GET);
+        $sql=$pdo->prepare("SELECT * from todos where id=:id order by complete");
+        $sql->execute(['id'=>$idtdodo]);
+        $todo_check=$sql->fetch();
+        $sql1=$pdo->prepare("UPDATE todos SET complete=:comp where id=:id");
+        $sql1->execute(['id'=>$idtdodo,'comp'=>$todo_check['complete'] ? 0 : 1]);
+     }
+     
      $sql=$pdo->prepare("SELECT * from todos where id_user=:id");
      $sql->execute(['id'=>$_SESSION['iduser']]);
      $alltodos=$sql->fetchall();
